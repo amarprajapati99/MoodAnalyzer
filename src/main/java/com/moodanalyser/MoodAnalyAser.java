40b77ea5d1f131d46bfeacd206b98b7fca971b3c
amarprajapati99 <amarprajapati99@gmail.com>
2021-04-05 21:00:16 +0530

    Given a message to analysis and respond happy or sad mood

--- src/main/java/com/moodanalyser/MoodAnalyAser.java
+++ src/main/java/com/moodanalyser/MoodAnalyAser.java
@@ -1,5 +1,10 @@
 package com.moodanalyser;
 
 public class MoodAnalyAser {
-   
+    public String analyaseMood(String message) {
+        if(message.contains("happy"))
+        return "happy";
+        else
+            return "sad";
+    }
 }
