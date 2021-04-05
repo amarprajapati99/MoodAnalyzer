3ea4c30197182577dbe8d005bda3d28e24d07ce1
amarprajapati99 <amarprajapati99@gmail.com>
2021-04-05 20:56:53 +0530

    Welcome

---
+++ src/test/java/com/moodanalyser/MoodAnayserTest.java
@@ -0,0 +1,27 @@
+package com.moodanalyser;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+
+public class MoodAnayserTest {
+
+//    @Test
+//    public  void givenMessgae_whenSad_shouldReturnSad(){
+//        MoodAnalyAser moodAnalyAser = new MoodAnalyAser();
+//        String mood =  moodAnalyAser.analyaseMood("this is happy mood");
+//        Assert.assertEquals("happy", mood);
+//
+//    }
+//    @Test
+//    public void givenMessgae_WhenHappy_shouldreturnHappy(){
+//        MoodAnalyAser moodAnalyAser = new MoodAnalyAser();
+//        String mood =  moodAnalyAser.analyaseMood("this is sad mood");
+//        Assert.assertEquals("sad", mood);
+
+
+
+
+    }
+
+}
